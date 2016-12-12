2b157ab922878eb215d56f0d3ef23b26d659e5a5
Shakaib Khan <shakaib97@gmail.com>
2016-12-11 19:22:32 -0500

    Connected to the bluetooth device on onCreate

---
+++ app/src/main/java/com/bewell/shakaibkhan/bluetoothmotor/ConnectBluetooth.java
@@ -0,0 +1,8 @@
+package com.bewell.shakaibkhan.bluetoothmotor;
+
+/**
+ * Created by shakaibkhan on 2016-12-11.
+ */
+
+public class ConnectBluetooth {
+}
--- app/src/main/java/com/bewell/shakaibkhan/bluetoothmotor/MainActivity.java
+++ app/src/main/java/com/bewell/shakaibkhan/bluetoothmotor/MainActivity.java
@@ -1,7 +1,12 @@
 package com.bewell.shakaibkhan.bluetoothmotor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(!mBluetoothAdapter.isEnabled()){
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth,1);
+        }
+        //Getting the connected devices as a set
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        BluetoothDevice mDevice;
+        if(pairedDevices.size() > 0){
+            for(BluetoothDevice device: pairedDevices){
+                mDevice = device;
+            }
+        }
+
     }
 }
